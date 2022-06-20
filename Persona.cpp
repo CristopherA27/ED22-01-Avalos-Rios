b57d2cc271a10145887ad3c17c61aab86a3ad097
CristopherA27 <76230505+CristopherA27@users.noreply.github.com>
2022-06-19 20:13:05 -0400

    actualizaciom

---
+++ Persona.cpp
@@ -0,0 +1,33 @@
+#include "Persona.hpp"
+
+Persona::Persona(cv::Rect &r){//Persona recive un rectangulo
+    xComienzo = r.x;
+    yComienzo = r.y;
+    xFin = r.x + r.width;
+    yFin = r.y + r.height;
+    xCentro = r.x + ((float)r.width/2.0);//calculo centroides
+    yCentro = r.y + ((float)r.height/2.0);;
+}
+int Persona::getXComienzo(){
+    return xComienzo;
+}
+
+int Persona::getYComienzo(){
+    return yComienzo;
+}
+
+int Persona::getXFin(){
+    return xFin;
+}
+
+int Persona::getYFin(){
+    return yFin;
+}
+
+int Persona::getXCentro(){
+    return xCentro;
+}
+
+int Persona::getYCentro(){
+    return yCentro;
+}
